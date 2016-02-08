b6aac78e03ed1a06101780f1d020d4a81c8588be
Pete Wilcox <petercwilcox@gmail.com>
2016-02-07 17:21:07 -0800

    all the methods are there, need to debug it a bit and maybe add an iterator?

--- src/DoubleNode.java
+++ src/DoubleNode.java
@@ -25,6 +25,7 @@ public class DoubleNode<Item> {
         size = 0;
     }
 
+    // Add a node to the front of the list
     public void addToFront(Item i) {
         Node temp = new Node();
         temp.item = i;
@@ -40,6 +41,7 @@ public class DoubleNode<Item> {
         System.out.println("Added " + temp.item + " to front, size is now " + size + ".");
     }
 
+    // Add a node to the back of the list
     public void addToBack(Item i) {
         Node temp = new Node();
         temp.item = i;
@@ -53,6 +55,7 @@ public class DoubleNode<Item> {
         System.out.println("Added " + temp.item + " to back, size is now " + size + ".");
     }
 
+    // Return the first node
     public Item popFromFront() {
         if (size > 0) {
             Node temp = first;
@@ -68,6 +71,7 @@ public class DoubleNode<Item> {
         }
     }
 
+    // Return the last node
     public Item popFromBack() {
         if (size > 0) {
             Node temp = last;
@@ -81,13 +85,136 @@ public class DoubleNode<Item> {
         }
     }
 
+    // Return true if the list is empty
     public boolean isEmpty() {
         return size == 0;
     }
 
+    // Return the number of nodes
     public int getSize() {
         return size;
     }
 
+    // Add a node before the nth node
+    public void addBefore(Item item, int n) {
+        if (n < 2) {
+            addToFront(item);
+        } else if (n > size) {
+            addToBack(item);
+        } else {
+            Node temp = new Node();
+            temp.item = item;
+            Node current = first;
+            for (int i = 1; i < n; i++) {
+                current = current.previous;
+            }
 
+            System.out.println("Adding " + temp.item.toString() + " before " + current.item.toString() + " and after " + current.next.item.toString() + ".");
+            temp.next = current.next;
+            temp.previous = current;
+            current.next.previous = temp;
+            current.next = temp;
+
+            size++;
+        }
+
+    }
+
+    // Add a node after the nth node
+    public void addAfter(Item item, int n) {
+        if (n < 1) {
+            addToFront(item);
+        } else if (n >= size) {
+            addToBack(item);
+        } else {
+            Node temp = new Node();
+            temp.item = item;
+            Node current = first;
+            for (int i = 1; i < n; i++) {
+                current = current.previous;
+            }
+
+            System.out.println("Adding " + temp.item.toString() + " after " + current.item.toString() + " and before " + current.previous.item.toString() + ".");
+            temp.next = current;
+            current.previous.next = temp;
+
+            temp.previous = current.previous;
+            current.previous = temp;
+
+            size++;
+        }
+    }
+
+    // Removes the nth element
+    public void remove(int n) {
+        if (n > 0 && n <= size) {
+            Node current = first;
+            for (int i = 1; i < n; i++) {
+                current = current.previous;
+            }
+
+            System.out.println("Removing " + current.item.toString() + ".");
+
+            if (first == current) {
+                if (current.previous != null) {
+                    first = current.previous;
+                } else {
+                    first = null;
+                }
+            } else if (last == current) {
+                current.next.previous = null;
+                last = current.next;
+            } else {
+                current.next.previous = current.previous;
+                current.previous.next = current.next;
+            }
+            current = null;
+            size--;
+
+
+        }
+    }
+
+    // Move the nth node to the front
+    public void moveToFront(int n) {
+        if (n > 1 && n <= size) {
+            Node current = first;
+            for (int i = 1; i <= n; i++) {
+                current = current.previous;
+            }
+
+            System.out.println("Moving " + current.item.toString() + " to the front of the list.");
+
+            if (current.previous != null) {
+                current.next.previous = current.previous;
+                current.previous.next = current.next;
+            } else {
+                current.next.previous = null;
+            }
+
+            current.previous = first;
+            current.next = null;
+            first.next = current;
+            first = current;
+        }
+    }
+
+    // Move the nth node to the back
+    public void moveToBack(int n) {
+        if (n > 0 && n < size) {
+            Node current = first;
+            for (int i = 1; i <= n; i++) {
+                current = current.previous;
+            }
+
+            System.out.println("Moving " + current.item.toString() + " to the back of the list.");
+
+            current.next.previous = current.previous;
+            current.previous.next = current.next;
+            current.previous = null;
+            current.next = last;
+            last.previous = current;
+            last = current;
+        }
+    }
 }
--- src/DoubleNodeTester.java
+++ src/DoubleNodeTester.java
@@ -4,12 +4,25 @@
 public class DoubleNodeTester {
     public static void main(String[] args) {
         DoubleNode list = new DoubleNode();
-        System.out.println("Made a list, let's add some stuff.");
+        System.out.println("Made a list.");
 
+        System.out.println("Currently the list is empty: " + list.isEmpty());
         list.addToFront("Dick");
         list.addToBack("Harry");
         list.addToFront("Tom");
 
+        System.out.println("Let's get the size, it's " + list.getSize() + ".");
+
+        list.addBefore("Jane", 2);
+        System.out.println("Size is now " + list.getSize() + ".");
+
+        list.addAfter("Vivian", 3);
+        System.out.println("Size is now " + list.getSize() + ".");
+
+        list.remove(3);
+
+        list.moveToBack(2);
+        list.moveToFront(3);
         list.popFromFront();
         list.popFromBack();
         list.popFromFront();
