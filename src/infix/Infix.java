f913b813310b8978629128bed3af62348f06c206
Pete Wilcox <petercwilcox@gmail.com>
2016-02-12 19:16:10 -0800

    comments

--- src/infix/Infix.java
+++ src/infix/Infix.java
@@ -11,17 +11,20 @@ public class Infix {
         operators = new Stack<>();
         operands = new Stack<>();
 
-        String[] input = exp.split("\s");
+        // Somehow split the string, not sure
+        // String[] input = exp.split('\s');
 
         for (String i : input) {
             if (i.equals("+") ||
                     i.equals("-") ||
                     i.equals("/") ||
                     i.equals("*")) {
+                // It's an operator, push it to the operator stack
                 operators.push(i);
             } else if (i.equals(")")) {
-                // calculate value of expression
+                // Pop operators/operands, calculate, push it to operand
             } else {
+                // It's a number, push it to the operand stack
                 operands.push((int)i);
             }
         }
