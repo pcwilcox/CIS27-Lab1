9ba9ed6985d3779f7a244936cf0064b5691086ca
pcwilcox <petercwilcox@gmail.com>
2016-02-25 16:30:55 -0800

    it worked and then it didn't

--- src/ArithmeticExpressionEvaluator/ArithmeticExpressionEvaluator.java
+++ src/ArithmeticExpressionEvaluator/ArithmeticExpressionEvaluator.java
@@ -19,10 +19,17 @@ public class ArithmeticExpressionEvaluator
         InfixToPostfix infix2 = new InfixToPostfix("( 1 + ( 3 * ( 4 / ( 8 - ( 12 + 5 ) * 18 ) ) / 4 ) - 6 )");
         System.out.println(infix2.toString());
 
-        Postfix postfix = new Postfix(infix2.getOutput());
+        Postfix postfix = new Postfix(infix2.toString());
 
         double value = postfix.getOutput();
 
+        System.out.println("The postfix output is: " + value);
+
+        System.out.println("Ok that worked, let's try another: ( 19 - 47 + ( 83 / ( 4 * 23 - 8 ) + 53 / 8 + 19 ) * 3 + 43 )");
+        InfixToPostfix infix3 = new InfixToPostfix("( 19 - 47 + ( 83 / ( 4 * 23 - 8 ) + 53 / 8 + 19 ) * 3 + 43 )");
+        System.out.println(infix3.toString());
+        Postfix postfix2 = new Postfix(infix3.toString());
+        value = postfix2.getOutput();
         System.out.println("The postfix output is: " + value);
     }
 }
--- src/ArithmeticExpressionEvaluator/Postfix.java
+++ src/ArithmeticExpressionEvaluator/Postfix.java
@@ -1,52 +1,49 @@
 package ArithmeticExpressionEvaluator;
 
+import java.util.Scanner;
+
 /**
  * Created by Lord Yod on 2/24/2016.
  */
 public class Postfix {
-    // This class takes a postfix expression, formatted as a stack of strings, and evaluates it to a double
-    private Stack<String> exp;
-    private Stack<String> operators;
+    // This class takes a postfix expression, formatted as a whitespace-delimited string, and evaluates it to a double
+    private Scanner input;
+    private String token;
     private Stack<Double> operands;
     private double output;
+    private double val;
 
-    Postfix(Stack<String> input) {
-        exp = input;
-        operators = new Stack();
+    public Postfix(String exp) {
+        input = new Scanner(exp);
+
         operands = new Stack();
 
-        // First we sort the input into operators and operands
-        while (!exp.isEmpty()) {
-            if (exp.getFirst().equals("+")
-                    || exp.getFirst().equals("-")
-                    || exp.getFirst().equals("*")
-                    || exp.getFirst().equals("/")) {
-                // If the element is an operator, it gets pushed to the operator stack
-                System.out.println("Pushing " + exp.getFirst() + " to operator stack.");
-                operators.push(exp.pop());
+        while (input.hasNext()) {
+            token = input.next();
 
-            } else {
-                // Anything that isn't an operator is an operand, so we convert it to a double and push it there
-                System.out.println("Pushing " + exp.getFirst() + " to operand stack.");
-                operands.push(new Double(exp.pop()));
-            }
-        }
+            if (token.equals("(")) {
 
-        // Then we iterate through the operators stack until it's empty
-        while (!operators.isEmpty()) {
-            String op = operators.pop();
-            Double val = operands.pop();
-            if (op.equals("+")) {
+            } else if (token.equals("+")) {
+                val = operands.pop();
                 val = operands.pop() + val;
-            } else if (op.equals("-")) {
+                operands.push(val);
+            } else if (token.equals("-")) {
+                val = operands.pop();
                 val = operands.pop() - val;
-            } else if (op.equals("*")) {
+                operands.push(val);
+            } else if (token.equals("*")) {
+                val = operands.pop();
                 val = operands.pop() * val;
-            } else {
+                operands.push(val);
+            } else if (token.equals("/")) {
+                val = operands.pop();
                 val = operands.pop() / val;
+                operands.push(val);
+            } else {
+                operands.push(new Double(token));
             }
-            operands.push(val);
         }
+
 
         output = operands.pop();
     }
